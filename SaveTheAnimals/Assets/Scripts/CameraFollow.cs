9afe6569eea370c7640275326d248c6b8c0a6e92
Jean Tan Li <30798045+JeanTL190@users.noreply.github.com>
2020-10-10 16:56:23 -0300

    Double Jump e teleport

---
+++ SaveTheAnimals/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Vector2 maxXandY;
+    [SerializeField] private Vector2 minXandY;
+    [SerializeField] private float smoothSpeed = 0.125f;
+    [SerializeField] private Transform player;
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        offset = transform.position - player.position;
+    }
+    private void FixedUpdate()
+    {
+        if (player)
+        {
+            /*float x = Mathf.Clamp(player.position.x, minXandY.x, maxXandY.x);
+            float y = Mathf.Clamp(player.position.y, minXandY.y, maxXandY.y);
+            this.transform.position = new Vector3(x, y, this.transform.position.z);*/
+            Vector3 desiredPosition = player.position + offset;
+            Vector3 SmoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+            transform.position = SmoothedPosition;
+        }
+    }
+}
--- SaveTheAnimals/Assets/Scripts/Espinhos.cs
+++ SaveTheAnimals/Assets/Scripts/Espinhos.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Espinhos : MonoBehaviour
 {
     [SerializeField] private LayerMask platformLayerMask;
+    [SerializeField] private Transform checkpoint;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision != null && (((1 << collision.gameObject.layer) & platformLayerMask) != 0))
         {
+            collision.GetComponent<TeleportPlayer>().TeleportarPlayer(checkpoint);
         }
     }
 
