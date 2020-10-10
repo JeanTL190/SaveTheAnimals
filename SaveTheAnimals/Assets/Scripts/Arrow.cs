01ce544fad81f8690b710f99b282a62dcd2e9fb7
LucLordello <lucasllordello@hotmail.com>
2020-10-10 18:01:58 -0300

    Arrow Turret

---
+++ SaveTheAnimals/Assets/Scripts/Arrow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    [SerializeField]
+    private float arrowSpeed;
+    private Transform parent;
+    [SerializeField]
+    private float distanceTravel;
+
+    void Start()
+    {
+        parent = gameObject.transform.parent.transform;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(arrowSpeed, 0);
+    }
+    
+    void Update()
+    {
+        if(Mathf.Abs(Vector2.Distance(parent.position, gameObject.transform.position)) > distanceTravel)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ SaveTheAnimals/Assets/Scripts/Turret.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [SerializeField]
+    private Transform arrowShooter;
+    [SerializeField]
+    private GameObject arrow;
+    [SerializeField]
+    private float startTime;
+    private float timeBetweenArrows;
+
+    void Start()
+    {
+        timeBetweenArrows = startTime;
+    }
+
+    void Update()
+    {
+        if(timeBetweenArrows <= 0)
+        {
+            Instantiate(arrow, arrowShooter.position, Quaternion.identity, gameObject.transform);
+            timeBetweenArrows = startTime;
+        }else
+        {
+            timeBetweenArrows -= Time.deltaTime;
+        }
+    }
+}
