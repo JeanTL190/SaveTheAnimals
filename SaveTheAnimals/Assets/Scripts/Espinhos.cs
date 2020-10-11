630edad421f8a39971493731680f9654060bbdf2
Jean Tan Li <30798045+JeanTL190@users.noreply.github.com>
2020-10-10 21:07:38 -0300

    Apagando debugs

--- SaveTheAnimals/Assets/Scripts/Espinhos.cs
+++ SaveTheAnimals/Assets/Scripts/Espinhos.cs
@@ -7,12 +7,9 @@ public class Espinhos : MonoBehaviour
     [SerializeField] private LayerMask platformLayerMask;
     [SerializeField] private Transform checkpoint;
     private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("Colidiu "+ collision.gameObject.layer, collision.gameObject);
-        
+    {   
         if( (((1 << collision.gameObject.layer) & platformLayerMask) != 0))
         {
-            Debug.Log("Entrou no if");
             collision.gameObject.GetComponent<TeleportPlayer>().TeleportarPlayer(checkpoint);
         }
     }
