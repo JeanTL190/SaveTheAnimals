1d6a744421edd43ab674a32b3c5d74aefc707c9a
LucLordello <lucasllordello@hotmail.com>
2020-10-10 23:50:41 -0300

    Rescue Animals

---
+++ SaveTheAnimals/Assets/Scripts/RescueAnimals.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RescueAnimals : MonoBehaviour
+{
+    bool permit = false;
+    GameObject animal;
+
+    void Update()
+    {
+        if (permit && animal != null)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //Aqui os animais desaparecem(sao salvos)
+                Destroy(animal);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Animal"))
+        {
+            permit = true;
+            animal = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Animal"))
+        {
+            permit = false;
+            animal = null;
+        }
+    }
+}
