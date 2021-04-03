446f33dc3411a4501a5bfbae26d4614e616b53f5
GuilhermePinheiro <guipinheirosantos@gmail.com>
2021-04-03 01:52:25 -0300

    Cena de teste

---
+++ SaveTheAnimals/Assets/Scripts/BuscaLayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuscaLayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray rayH = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfoModel;
+
+        if(Physics.Raycast(rayH, out hitInfoModel))
+        {
+            Debug.Log(hitInfoModel);
+        }
+    }
+}
