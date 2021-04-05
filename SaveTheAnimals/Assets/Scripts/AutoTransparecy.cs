bac1b5ade5e26cf84d0918bf2ced62b1ae5179e3
Jean Tan Li <jeantl@outlook.com.br>
2021-04-04 23:14:15 -0300

    Sistema de segredos feito!

---
+++ SaveTheAnimals/Assets/Scripts/AutoTransparecy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class AutoTransparecy : MonoBehaviour
+{
+    [SerializeField] [Range(0, 1)] private float transparency;
+    [SerializeField] private Renderer targetRenderer;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] float fadeDuration;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(layerMask == (layerMask|(1 << collision.gameObject.layer)))
+        {
+            SetTransparency(transparency);
+        }
+    }
+    private void OnTriggerExit2D (Collider2D collision)
+    {
+        if (layerMask == (layerMask | (1 << collision.gameObject.layer)))
+        {
+            SetTransparency(1);
+        }
+    }
+    private void SetTransparency(float alpha)
+    {
+        StopCoroutine("FadeCoroutine");
+        StartCoroutine("FadeCoroutine",alpha);
+    }
+    private IEnumerator FadeCoroutine(float fadeTo)
+    {
+        float timer = 0;
+        Color currentColor = targetRenderer.material.color;
+        float startAlpha = targetRenderer.material.color.a;
+        while (timer < 1)
+        {
+            yield return new WaitForEndOfFrame();
+            timer += Time.deltaTime / fadeDuration;
+
+            currentColor.a = Mathf.Lerp(startAlpha, fadeTo, timer);
+            targetRenderer.material.color = currentColor;
+        }
+    }
+}
